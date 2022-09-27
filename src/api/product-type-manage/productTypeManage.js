bbc0e350cc4e790a5a96c808d34794b6837df8b9
zedzy <72534015+zedzy@users.noreply.github.com>
2022-09-28 00:07:53 +0800

    Delete productTypeManage.js

--- src/api/product-type-manage/productTypeManage.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function delProductType(params) {  // 删除产品类型
-    return request({
-      method: 'post',
-      url: 'http://8.134.56.88:8080/trade/tradeController/deleteProductType.do',
-      params
-    })
-}
